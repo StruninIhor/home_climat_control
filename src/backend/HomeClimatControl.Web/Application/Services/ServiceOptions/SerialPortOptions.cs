41bb7e231570ee4f5fc50a055dc64da0319b35b8
Ihor Strunin <strunin.ihor@gmail.com>
2020-04-30 23:00:40 +0300

    Another timing fix

--- src/backend/HomeClimatControl.Web/Application/Services/ServiceOptions/SerialPortOptions.cs
+++ src/backend/HomeClimatControl.Web/Application/Services/ServiceOptions/SerialPortOptions.cs
@@ -5,5 +5,6 @@
         public string SerialPortName { get; set; }
         public int BaudRate { get; set; }
         public int OkRetryCount { get; set; } = 3;
+        public int TimeToWaitOk { get; set; } = 2000;
     }
 }
